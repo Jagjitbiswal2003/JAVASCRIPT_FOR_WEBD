dbbe8854e3a90e26bfbd689245933c7adbbefdcd
Jagjit Biswal <157984815+Jagjitbiswal2003@users.noreply.github.com>
2024-03-18 19:57:03 +0530

    Delete Assignment_06.js

--- Assignment_06.js
+++
@@ -1,7 +0,0 @@
-//In this program we see how to use anonymous function !
-// Anonymous function - a function whose don't have any name. 
-
-   let name = function(name){   // Anonymous function declaration.
-         console.log("Hello "+name+" how are you!" );
-   }
-      name("jagjit");   // calling anonymous function.
