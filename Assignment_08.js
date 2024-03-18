8602cadde44143fb53c1def81afb8f50655eaa4e
Jagjit Biswal <157984815+Jagjitbiswal2003@users.noreply.github.com>
2024-03-18 19:57:39 +0530

    Delete Assignment_08.js

--- Assignment_08.js
+++
@@ -1,6 +0,0 @@
-//Write an arrow function that takes a string as input and returns a new string with 
-//all characters in uppercase.
-let toUpper_name = str => {
-       return str.toUpperCase();
-}
-  console.log("My nmae in uppercase is "+toUpper_name("hello jagjit"));
