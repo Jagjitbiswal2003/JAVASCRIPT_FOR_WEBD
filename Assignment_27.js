c91acfc38c4853fc939be73ef6cacb13ca83d3c3
Jagjit Biswal <157984815+Jagjitbiswal2003@users.noreply.github.com>
2024-06-06 22:56:13 +0530

    Serially execute the Async code

---
+++ Assignment_27.js
@@ -0,0 +1,30 @@
+// Serialy execute the async code
+
+const fs = require("fs")
+
+
+console.log("Hi from JS")
+
+// File reading and the function check1 and check2 are run in asychronouslly.
+
+let data = fs.readFile("File.txt",check1)
+
+function check1(err,data){
+     if(err)
+        console.log("The server get an error"+err)
+    else
+    console.log("The content of Data:"+data)
+     
+    let data1 = fs.readFile("File1.txt",check2)
+
+}
+
+
+function check2(err,data1){
+     if(err)
+        console.log("The server get an error"+err)
+     else
+     console.log("The content of Data:"+data1)
+}
+
+console.log("Again visit to my JS server,Thank you!")
